5843fc8dad73db166ba2176de486b2e39eb0b67c
Johanny de Lucena Santos <johanny.dls@gmail.com>
2020-05-13 19:00:44 -0300

    Conecta Intervalo com redux

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 
 import './Intervalo.css';
+import { connect } from 'react-redux';
 
 import Card from './Card';
 
-export default props => {
+function Intervalo(props) {
+
+  const { min, max } = props;
 
   return (
     <Card title="Intervalo de Números" red>
       <div className="Intervalo">
         <span>
           <strong>Mínimo:</strong>
-          <input type="number" value={0} readOnly/>
+          <input type="number" value={min} readOnly/>
         </span>
         <span>
           <strong>Máximo:</strong>
-          <input type="number" value={10} readOnly/>
+          <input type="number" value={max} readOnly/>
         </span>
       </div>
     </Card>
   )
 }
+
+function mapStateToProps({numeros}) {
+  return {
+    min: numeros.min,
+    max: numeros.max
+  }
+}
+
+export default connect(mapStateToProps)(Intervalo);
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -2,14 +2,13 @@ import { createStore, combineReducers } from 'redux';
 
 const reducers = combineReducers({
   numeros: function(state, action) {
-    // console.log(state, ' ', action);
+    console.log(state, ' ', action);
     return {
       min: 7,
       max: 31
     }
   },
   nomes: function(state, action) {
-    // console.log(state, ' ', action);
     return [
       'Ana',
       'Maria',
